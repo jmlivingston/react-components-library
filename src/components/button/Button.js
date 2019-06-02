dd2447a92e81ab6b56db582960353c85797725c3
John Livingston <jmlivingston@gmail.com>
2019-06-02 14:30:33 -0700

    Add parcel for build

---
+++ src/components/button/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+function Button({ onClick, text }) {
+  const test = { id: 1 }
+  const test2 = { ...test, name: 'foo' }
+  return (
+    <button className="button" onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Button
