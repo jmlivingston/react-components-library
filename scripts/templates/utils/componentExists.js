f5b75c2ccdb084218988796c36df6f96ea7799ce
John Livingston <jmlivingston@gmail.com>
2019-06-09 13:38:10 -0700

    Remove code used for analysis

--- scripts/templates/utils/componentExists.js
+++ scripts/templates/utils/componentExists.js
@@ -1,9 +1,3 @@
-/**
- * componentExists
- *
- * Check whether the given component exist in either the components or containers directory
- */
-
 const fs = require('fs')
 const path = require('path')
 const pageComponents = fs.readdirSync(path.join(__dirname, '../../../app/components'))
